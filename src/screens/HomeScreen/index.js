227155911591b7d3548966d570e6845f8d8c822a
dm-mensavi <mensavidavid@gmail.com>
2023-06-16 17:45:45 +0000

    finish setting up environment

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const HomeScreen = () => {
+  return(
+    <View>
+      <Text>Hello and welcome to my home screen. 🤗</Text>
+    </View>
+  )
+};
+
+export default HomeScreen;
